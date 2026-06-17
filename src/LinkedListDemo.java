936e0d26e21b7885ab6fafd73f69f2d170a1435d
Tejas Shah <tejasshah2k19@gmail.com>
2026-06-17 16:26:30 +0530

    single ll

--- src/LinkedListDemo.java
+++ src/LinkedListDemo.java
@@ -6,6 +6,12 @@ public class LinkedListDemo {
 		sl.addNode(10);
 		sl.addNode(20);
 		sl.addNode(30);
+		sl.addNodeBEG(5); // 5 10 20 30
+		sl.display();
+
+		sl.search(110); // not found
+		sl.search(20); // found
+
 	}
 }
 
@@ -26,14 +32,70 @@ class SingleLinkedList {
 			tmp.next = null;
 			last.next = tmp;
 			last = tmp;
-			
 		}
 	}
 
+	public void addNodeBEG(int data) {
+		Node tmp = new Node();
+		tmp.data = data;
+		tmp.next = head;
+		head = tmp;
+	}
+
 	void display() {
+		Node p = head;
+
+		while (p != null) {
+			System.out.println(p.data);// 10
+			p = p.next;
+		}
 
 	}
+
+	void search(int data) {
+		Node p = head;
+		boolean flag = false;
+
+		while (p != null) {
+			if (p.data == data) {
+				flag = true;
+			}
+			p = p.next;
+		}
+
+		if (flag) {
+			System.out.println(data + " found ");
+		} else {
+			System.out.println(data + " not found");
+		}
+	}
+
+	void delBeg() {
+		Node p = head;
+		head = head.next;
+		p = null;
+	}
+
+	void delLast() {
+		Node p = head; 
+		
+		while(p.next != last) {
+			p=p.next;
+		}
+		
+		p.next = null;
+		last =p;
+	}
+	
+	void addAny(int source,int data) {//30,90  44,90=>invalid source
+		
+	}
+	
+	void delAny(int source) {
+		
+	}
 }
+
 
 class Node {
 	int data;
